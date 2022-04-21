17205f2bd7a5f169bfd0c7bfa40af487b62c4db1
Darius <dariusmmcclendon@gmail.com>
2022-04-21 18:41:17 -0400

    Initial commit

---
+++ migrations/20220420165759-create-band.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('bands', {
+      band_id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      genre: {
+        type: Sequelize.TEXT,
+        allowNull: false
+      },
+      available_start_time: {
+        type: Sequelize.DATE
+      },
+      end_time: {
+        type: Sequelize.DATE
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Bands');
+  }
+};
